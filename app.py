1d5514c9dae19df0e1cf62522eb8c9589afb0275
boscot553 <yizhengtian2011@gmail.com>
2025-10-03 09:49:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-import json
-## Open the JSON file of movie data
-movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
-data = json.load(movies)
+# import json
+# movies = open("./movies.json", encoding="utf8")
+# data = json.load(movies)
+# # num = 0
+# # for i in range(14117):
+# #     x = data[num]["title"]
+# #     print(x)
+# #     num += 1
 # num = 0
-# for i in range(14117):
-#     x = data[num]["title"]
-#     print(x)
-#     num += 1
-
-num = 0
-y = data[num]["year"]
-year = input("Movies after this year  ")
-year = str(year)
-y = str(y)
-print(y)
-#for i in range(14117):
-    
+# y = data[num]["year"]
+# year = input("Movies after this year  ")
+# year = str(year)
+# y = str(y)
+# #for i in range(14117):
     
 
+x = input("Give a sentence in either french or english ")
+y = x.count('t')
+z = x.count('T')
+Ts = y + z
+v = x.count('s')
+w = x.count('S')
+Ss = v + w
+if Ts > Ss:
+    print("English")
+elif Ss > Ts:
+    print("French")
