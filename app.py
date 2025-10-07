83ed3c9f00e7ed4b336e71d628e5fbe0efaa33a4
boscot553 <yizhengtian2011@gmail.com>
2025-10-07 10:16:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 import json
 movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
-num = 0
-for i in range(14117):
-    x = data[num]["title"]
-    print(x)
-    num += 1
+
+
+# for i in range(14117):
+#     x = data[num]["title"]
+#     print(x)
+#     num += 1
+# num = 0
+
+year = input("Movies after this year  ")
+year = int(year)
 num = 0
 y = data[num]["year"]
-year = input("Movies after this year  ")
-year = str(year)
-y = str(y)
-#for i in range(14117):
-    
+y = int(y)
+
+for i in range(14116.5):
+    if year < y:
+        print(data[num]["title"])
+        num += 1
+        if num == "14117":
+            break
+        y = data[num]["year"]
+        y = int(y)
+    elif year > y or year == y:
+        num += 1
+        if num == "14117":
+            break
+        y = data[num]["year"]
+        y = int(y)
+        
+
+
 
 
