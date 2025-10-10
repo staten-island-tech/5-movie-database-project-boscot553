fa8d7c3171f6e656de1263a24235e047bfb6939b
boscot553 <yizhengtian2011@gmail.com>
2025-10-10 10:17:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -76,72 +76,26 @@ data = json.load(movies)
 #     else:
 #         num += 1
 
-# def genre()
-# num = 0
-# for i in range(14117):
-#     if genre == data[num]["genres"]:
-#         print(data[num]["title"])
-#         num += 1
-#     else:
-#         num += 1
+
+num = 0
+genre = input("What genre?  ")
+for i in range(14117):
+    if genre == data[num]["genres"][0]:
+        print(data[num]["title"])
+        num += 1
+    elif genre == data[num]["genres"][1]:
+        print(data[num]["title"])
+        num += 1
+    elif genre == data[num]["genres"][2]:
+        print(data[num]["title"])
+        num += 1
+    elif genre == data[num]["genres"][3]:
+        print(data[num]["title"])
+        num += 1
+    elif genre == data[num]["genres"][4]:
+        print(data[num]["title"])
+        num += 1
+    else:
+        num += 1
 
 
-def honi(x):
-    num = 0
-    HONIS = 0
-    for char in x:
-        for char in x:
-            if x[num] == "H":
-                break
-            else:
-                num += 1
-        for char in x:
-            if x[num] == "O":
-                break
-            else:
-                num += 1   
-        for char in x:
-             if x[num] == "N":
-                break
-             else:
-                num += 1           
-        for char in x:
-            if x[num] == "I":
-                HONIS += 1
-                break
-            else:
-                num += 1           
-    print(HONIS)
-    
-honi("HONIS")
-
-
-
-        
-
-
-
-
-# def honi(x):
-#     num = 0
-#     y = x[0]
-#     for char in x:
-#         y = x[num]
-#         num += 1
-#         if y == "H":
-#             for char in len(x) - num:
-#                 y = x[num]
-#                 num += 1
-#                 if y == "O":
-#                     for char in x - num:
-#                         y = x[num]
-#                         num += 1
-#                         if y =="N":
-#                             for char in x - num:
-#                                 y = x[num]
-#                                 num += 1
-#                                 if y =="I":
-#                                     for char in x - num:
-#                                         y = x[num]
-#                                         num += 1
-#                                         HONIS += 1
