e88ab87cd11c87c4a145b0e2a1c5d831961651a9
boscot553 <yizhengtian2011@gmail.com>
2025-10-17 09:36:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -75,60 +75,11 @@ data = json.load(movies)
 #         break
 #     else:
 #         num += 1
-
-
-# x = input("Genre  ")
-# num = 0
-# for i in range(14117):
-#     y = data[num]["genres"]
-#     z = len(y)
-#     if z == 1:
-#         if x == y[0]:
-#             print(data[num]["title"])
-#             num += 1
-#         else:
-#             num += 1
-#     if z == 2:
-#         if x == y[0] or x == y[1]:
-#             print(data[num]["title"])
-#             num += 1
-#         else:
-#             num += 1
-#     if z == 3:
-#         if x == y[0] or x == y[1] or x == y[2]:
-#             print(data[num]["title"])
-#             num += 1
-#         else:
-#             num += 1
-#     if z == 4:
-#         if x == y[0] or x == y[1] or x == y[2] or x == y[3]:
-#             print(data[num]["title"])
-#             num += 1
-#         else:
-#             num += 1
-#     if z == 5:
-#         if x == y[0] or x == y[1] or x == y[2] or x == y[3] or x == y[4]:
-#             print(data[num]["title"])
-#             num += 1
-#         else:
-#             num += 1
-#     if z == 6:
-#         if x == y[0] or x == y[1] or x == y[2] or x == y[3] or x == y[4] or x == y[5]:
-#             print(data[num]["title"])
-#             num += 1
-#         else:
-#             num += 1
         
 num = 0
 x = input("Genre  ")
 for i in range(14116):
-    z = 0
     num += 1
     y = data[num]["genres"]
-    for str in y:
-        if x == y[z]:
-            print(data[num]["title"])
-            break
-        else:
-            z += 1
-        
+    if x in y:
+        print(data[num]["title"])
