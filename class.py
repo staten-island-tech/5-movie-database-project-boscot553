acd2dce836349ce13ebf1ad1aff935d071141368
boscot553 <yizhengtian2011@gmail.com>
2025-10-21 10:17:09 -0400

    Create class.py

---
+++ class.py
@@ -0,0 +1,10 @@
+class pet:
+    def __init__(self, food, water):
+        self.food = food
+        self.water = water
+    def feed(x, y):
+        food += x
+        water += y
+
+pet("dog", "10")
+pet.self
