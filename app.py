abc38d85e36198ccef2af50fb304ff9101c9878b
boscot553 <yizhengtian2011@gmail.com>
2025-10-23 09:55:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import json
-movies = open("./movies.json", encoding="utf8")
-data = json.load(movies)
+# import json
+# movies = open("./movies.json", encoding="utf8")
+# data = json.load(movies)
 # num = 0
 
 # for i in range(14117):
@@ -53,33 +53,41 @@ data = json.load(movies)
 # y = data[num]["year"]
 # y = int(y)
 
-# for i in range(14117):
-#     if year == y:
-#         print(data[num]["title"])
-#         num += 1
-#         num = int(num)
-#         y = data[num]["year"]
-#         y = int(y)
-#     elif year < y or year > y:
-#         num += 1
-#         if num < int("14117"):
-#             y = data[num]["year"]
-#             y = int(y)
 
-
-# pro = input("Search for a movie  ")
-# num = 0
-# for i in range(14117):
-#     if pro == data[num]["title"]:
-#         print(data[num])
-#         break
-#     else:
-#         num += 1
         
-num = 0
-x = input("Genre  ")
-for i in range(14116):
-    num += 1
-    y = data[num]["genres"]
-    if x in y:
-        print(data[num]["title"])
+# num = 0
+# x = input("Genre  ")
+# for i in range(14116):
+#     num += 1
+#     y = data[num]["genres"]
+#     if x in y:
+#         print(data[num]["title"])
+
+
+def slots(q, x, y, z):
+    plays = 0
+    while q > 0:
+        if x < 35 and q > 0:
+            x += 1
+            plays += 1
+            q -= 1
+        else:
+            q += 30
+            x -= 35
+        if y < 100 and q > 0:
+            y += 1
+            plays += 1
+            q -= 1
+        else:
+            q += 60
+            y -= 100
+        if z < 10 and q > 0:
+            z += 1
+            plays += 1
+            q -= 1
+        else:
+            q += 9
+            z -= 10
+    print(f"Martha plays {plays} times before going broke")
+
+slots(77, 4, 9, 3)
